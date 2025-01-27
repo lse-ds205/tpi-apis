81516ac3a744cd3236292c0ec56bf49cb6b4d750
Jon Cardoso-Silva <j.cardoso-silva@lse.ac.uk>
2025-01-27 16:36:49 +0000

    🐞  Set up FastAPI differently if running from Nuvolos

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.app import app
-
-main_app = FastAPI()
-main_app.mount("/v1", app)  # your app routes will now be /v1/{your-route-here}
+from v1.app import app
 
 if __name__ == "__main__":
-    uvicorn.run(main_app, host="127.0.0.1", log_level="info")
+    uvicorn.run(app, host="127.0.0.1", log_level="info")
--- v1/app.py
+++ v1/app.py
@@ -1,6 +1,25 @@
+import os
+
 from fastapi import FastAPI
 
-app = FastAPI()
+def __is_running_on_nuvolos():
+    """
+    If we are running this script from Nuvolos Cloud, 
+    there will be an environment variable called HOSTNAME
+    which starts with 'nv-'
+    """
+
+    hostname = os.getenv("HOSTNAME")
+    return hostname is not None and hostname.startswith('nv-')
+
+if __is_running_on_nuvolos():
+    # Nuvolos alters the URL of the API (likely for security reasons)
+    # Instead of https://A-BIG-IP-ADDRESS:8000/
+    # The API is actually served at https://A-BIG-IP-ADDRESS/proxy/8000/
+    app = FastAPI(root_path="/proxy/8000/")
+else:
+    # No need to set up anything else if running this on local machine
+    app = FastAPI()
 
 @app.get("/")
 async def read_root():
