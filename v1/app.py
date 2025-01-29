65701bcbcea8238ccf0202aa56b2b50c96ddd6f1
datascraper758 <m.silvestri1@lse.ac.uk>
2025-01-29 12:46:06 +0000

    (WIP) Add a first version of the country endpoint

--- v1/app.py
+++ v1/app.py
@@ -1,6 +1,12 @@
 import os
-
+import pandas as pd
 from fastapi import FastAPI
+
+file_path = './data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx'
+df_assessments = pd.read_excel(file_path)
+df_assessments['Publication date'] = pd.to_datetime(df_assessments['Publication date'], format='%d/%m/%Y')
+df_assessments['Assessment date'] = pd.to_datetime(df_assessments['Assessment date'], format='%d/%m/%Y')
+
 
 def __is_running_on_nuvolos():
     """
@@ -24,3 +30,39 @@ else:
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/v1/country-data/{country}/{assessment_year}")
+async def get_country_data(country: str, assessment_year: int):
+
+    selected_row = (
+        (df_assessments["Country"] == country) &
+        (df_assessments['Assessment date'].dt.year == assessment_year)
+    )
+
+    # Filter the data
+    data = df_assessments[selected_row]
+
+    # Selected and filter columns
+    area_columns = [col for col in df_assessments.columns if col.startswith("area")]
+    data = data[area_columns]
+    
+    # JSON does not allow for NaN or NULL. 
+    # The equivalent is just to leave an empty string instead
+    data = data.fillna('')
+
+    #Rename columns
+    data['country'] = country
+    data['assessment_year'] = assessment_year
+
+    remap_area_column_names = {
+        col: col.replace('area ', '')
+        for col in area_columns
+    }
+
+    data = data.rename(columns=remap_area_column_names)
+
+    # Grab just the first element (there should only be one anyway)
+    # and return it as a dictionary
+    output_dict = data.iloc[0].to_dict()
+
+    return output_dict
