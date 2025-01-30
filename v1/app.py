f321d72d8041371656785a770b981cdce7a2325d
Eden <hansrichard.eden@gmail.com>
2025-01-30 11:26:24 +0000

    Add a first version of the country endpoint

--- v1/app.py
+++ v1/app.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+import numpy as np
 
 from fastapi import FastAPI
 
@@ -24,3 +26,41 @@ else:
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+
+@app.get("/v1/country-data/{country}/{assessment_year}")
+async def get_country_data(country: str, assessment_year: int):
+    
+    # Load the data
+    df = pd.read_excel("./data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx")
+
+    # Convert the date columns to datetime type
+    df['Assessment date'] = pd.to_datetime(df['Assessment date'], format='%d/%m/%Y')
+    df['Publication date'] = pd.to_datetime(df['Publication date'], format='%d/%m/%Y')
+
+    # Keep only relevant columns
+    df = df[[
+        "area EP.1", "area EP.2", "area EP.3", "area CP.1", "area CP.2", "area CP.3", 
+        "area CP.4", "area CP.5", "area CP.6", "area CF.1", "area CF.2", "area CF.3", 
+        "area CF.4", "Country", "Assessment date"
+    ]]
+
+    # Only keep the year in the "Assessment date" column
+    df['Assessment date'] = df['Assessment date'].dt.year #converts the column entries to integers!
+
+    # Filter for relevant rows
+    df = df.loc[(df["Country"] == country) & (df["Assessment date"] == assessment_year)]
+
+    # Filter out all NaNs
+    df.replace(np.nan, "", inplace=True)
+
+    # Rename the columns to remove the "area " prefix
+    df.rename(columns=lambda x: x.replace("area ", ""), inplace=True)
+    df.rename(columns={"Country": "country", "Assessment date": "assessment_year"}, inplace=True)
+
+    # Convert dataframe to dictionary
+    my_dict = df.to_dict(orient="records")
+
+    # Silly non-data-driven response for now
+    return my_dict
