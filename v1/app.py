4bdf858deb6244f904834f444b6e723d7fa1b093
felix-brown <felixgibsonbrown@gmail.com>
2025-01-31 16:51:20 +0000

    (WIP) Add a first version of the country endpoint

--- v1/app.py
+++ v1/app.py
@@ -1,7 +1,10 @@
 import os
 import pandas as pd
 
+
 from fastapi import FastAPI
+from .models import CountryData 
+
 app = FastAPI()
 
 # from .models import CountryData 
@@ -22,12 +25,13 @@ async def get_country_data(country: str, assessment_year: int):
     #only include country, year row
     mask = (df_assessments['Assessment date'].dt.year == assessment_year) & (df_assessments['Country'] == country)
     filtered_df = df_assessments[mask]
+    filtered_df = filtered_df.iloc[: , 1:10] #####test here to see if missing values cause errors
 
     #remove irrelevant columns
     filtered_df = filtered_df.drop(["Id", "Assessment date", "Publication date", "Country Id", "Country"], axis=1)
 
     #return results as a dictionary
-    df_dict = filtered_df.to_dict(orient='records')
+    df_dict = filtered_df.iloc[0].to_dict()
 
     return {
         "message": f"You requested data for {country} in {assessment_year}",
