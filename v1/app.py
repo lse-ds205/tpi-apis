1473665ebc494071f5311ff8001e33d9ed29091e
nayrbnat <nayrbnat@gmail.com>
2025-02-02 17:05:21 +0000

    Add basic Pydantic model for API response

--- v1/app.py
+++ v1/app.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from fastapi import FastAPI, HTTPException
 import numpy as np
+from .models import CountryData 
 
 app = FastAPI()
 filepath = "./data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx"
@@ -9,41 +10,42 @@ df_assessments = pd.read_excel(filepath, engine='openpyxl')
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "No"} 
+    return {"Hello": "Yes"} 
 
-@app.get("/v1/country-data/{country}/{assessment_year}")
-async def get_country_data(country: str, assessment_year: int):
-    try:
+@app.get("/v1/country-data/{country}/{assessment_year}", response_model=CountryData)
+async def get_country_data(country: str, assessment_year: int) -> CountryData:
+    try:        
         # Convert dates
         df_assessments['Assessment date'] = pd.to_datetime(df_assessments['Assessment date'])
-        df_assessments['Publication date'] = pd.to_datetime(df_assessments['Publication date'])
-        
-        # Case-insensitive country match
+        # Filter data
         mask = (
             (df_assessments['Country'].str.lower() == country.lower()) & 
             (df_assessments['Assessment date'].dt.year == assessment_year)
         )
         filtered_df = df_assessments[mask]
         
-        # Check if any data exists
         if filtered_df.empty:
             raise HTTPException(status_code=404, detail="Data not found")
+        # Selected and filter columns
+        area_columns = [col for col in df_assessments.columns if col.startswith("area")]
+        filtered_df = filtered_df[area_columns]
+        filtered_df = filtered_df.fillna('')
+        # Clean and transform data
+        #Rename columns
+        filtered_df['country'] = country
+        filtered_df['assessment_year'] = assessment_year
         
-        # Column selection and cleaning
-        filtered_df = filtered_df.iloc[:, 1:10].drop(
-            columns=["Id", "Assessment date", "Publication date", "Country Id", "Country"],
-            errors='ignore'  # Ignore if columns don't exist
-        )
-        
-        # Convert to dict
-        df_dict = filtered_df.iloc[0].to_dict()
-        
-        return {
-            "message": f"You requested data for {country} in {assessment_year}",
-            "data": df_dict
+        remap_area_column_names = {
+            col: col.replace('area ', '').replace('.', '_')
+            for col in area_columns
         }
-        
+
+        filtered_df = filtered_df.rename(columns=remap_area_column_names)
+        output_dict = filtered_df.iloc[0].to_dict()
+        output = CountryData(**output_dict)
+        return output
     except HTTPException as he:
         raise he
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
