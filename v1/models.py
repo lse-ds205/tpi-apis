1f884def35efe72c0b26bde3d8cf1fc895048f5d
nayrbnat <nayrbnat@gmail.com>
2025-02-02 18:02:33 +0000

    Hierarchy

--- v1/models.py
+++ v1/models.py
@@ -6,19 +6,7 @@ class Area(BaseModel):
 
 class CountryData(BaseModel):
     country: str
-    # Only allow years between 2023-2024
-    assessment_year: int = Field(ge=2023, le=2024)
-    # Only allow specific values for assessment results
-    EP_1: Literal["Yes", "No", "Partial", ""] 
-    EP_2: Literal["Yes", "No", "Partial", ""]
-    EP_3: Literal["Yes", "No", "Partial", ""]
-    CP_1: Literal["Yes", "No", "Partial", ""]
-    CP_2: Literal["Yes", "No", "Partial", "", "Exempt"]
-    CP_3: Literal["Yes", "No", "Partial", "", "Exempt"]
-    CP_4: Literal["No", "Partial", "", "Exempt"]
-    CP_5: Literal["Yes", "No", "Partial", ""]
-    CP_6: Literal["Yes", "No", "Partial", ""]
-    CF_1: Literal["Yes", "No", "Partial", "", "Exempt"]
-    CF_2: Literal["Yes", "No", "Partial", "", "Exempt"]
-    CF_3: Literal["Yes", "No", "Partial", ""]
-    CF_4: Literal["Not applicable", "nan",""]
+    assessment_year: int
+    EP: Area
+    CP: Area
+    CF: Area
