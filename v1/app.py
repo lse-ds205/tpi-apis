5de21dd4e89e61b99533b5af0bd8f0254c6809e4
nikaisya <N.A.Binti-Nik-Abdullah-Shukri@lse.ac.uk>
2025-02-03 23:02:59 +0000

    Add initial data validation to the Pydantic model

--- v1/app.py
+++ v1/app.py
@@ -3,8 +3,6 @@ import pandas as pd
 from fastapi import FastAPI, HTTPException
 from .models import CountryData 
 
-app = FastAPI()
-
 def __is_running_on_nuvolos():
     """
     If we are running this script from Nuvolos Cloud, 
@@ -31,7 +29,7 @@ async def read_root():
 @app.get("/v1/country-data/{country}/{assessment_year}", response_model=CountryData)
 async def get_country_data(country: str, assessment_year: int) -> CountryData:
    
-   # Load the data in ASCOR_assessment_results.xlsx
+    # Load the data in ASCOR_assessment_results.xlsx
     filepath = "./data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx" # Specify the correct path to the file
     df_assessments = pd.read_excel(filepath)
 
@@ -40,56 +38,34 @@ async def get_country_data(country: str, assessment_year: int) -> CountryData:
     df_assessments['Publication date'] = pd.to_datetime(df_assessments['Publication date'])
 
     # Filter the data for the specified country and year
-    df_filtered = df_assessments[(df_assessments['Country'] == country) & (df_assessments['Assessment date'].dt.year == assessment_year)]
+    requirement = (df_assessments['Country'] == country) & (df_assessments['Assessment date'].dt.year == assessment_year)
+    df_filtered = df_assessments[requirement]
 
-    if df_filtered.empty:
-        raise HTTPException(status_code=404, detail="Data not found for the specified country and year")
-    
+    # if df_filtered.empty:
+    #     raise HTTPException(status_code=404, detail="Data not found for the specified country and year")
+
     # Extract the relevant columns
-    key_columns = [col for col in df_filtered.columns if col.startswith(('area EP.', 'area CP.', 'area CF.', 'indicator EP.', 'indicator CP.', 'indicator CF.')) and not any(suffix in col for suffix in ['.a', '.b', '.c', '.d', '.e', '.i', '.ii'])]
-    # output_dict = df_filtered[key_columns].iloc[0].to_dict()
-    df_filtered = df_filtered[key_columns]
+    area_columns = [col for col in df_filtered.columns if col.startswith(('area')) and not any(suffix in col for suffix in ['.a', '.b', '.c', '.d', '.e', '.i', '.ii'])]
 
-    # Rename columns to match the expected keys
-    df_filtered.rename(columns={
-        'area EP.1': 'EP.1',
-        'area EP.2': 'EP.2',
-        'area EP.3': 'EP.3',
-        'area CP.1': 'CP.1',
-        'area CP.2': 'CP.2',
-        'area CP.3': 'CP.3',
-        'area CP.4': 'CP.4',
-        'area CP.5': 'CP.5',
-        'area CP.6': 'CP.6',
-        'area CF.1': 'CF.1',
-        'area CF.2': 'CF.2',
-        'area CF.3': 'CF.3',
-        'area CF.4': 'CF.4'
-    }, inplace=True)
+    # JSON does not allow for NaN or NULL. 
+    # The equivalent is just to leave an empty string instead
+    df_filtered = df_filtered.fillna('')
 
-    output_dict = df_filtered.iloc[0].to_dict()
-
-    # Replace missing values with an empty string
-    output_dict = {k: (v if pd.notna(v) else "") for k, v in output_dict.items()}
 
     # Add country and assessment_year to the result
-    output_dict['country'] = country
-    output_dict['assessment_year'] = assessment_year
-
-    output = CountryData(country=output_dict['country'],
-                         assessment_year=output_dict['assessment_year'],
-                         EP_1=output_dict['EP.1'],
-                         EP_2=output_dict['EP.2'],
-                         EP_3=output_dict['EP.3'],
-                         CP_1=output_dict['CP.1'],
-                         CP_2=output_dict['CP.2'],
-                         CP_3=output_dict['CP.3'],
-                         CP_4=output_dict['CP.4'],
-                         CP_5=output_dict['CP.5'],
-                         CP_6=output_dict['CP.6'],
-                         CF_1=output_dict['CF.1'],
-                         CF_2=output_dict['CF.2'],
-                         CF_3=output_dict['CF.3'],
-                         CF_4=output_dict['CF.4'])
+    df_filtered['country'] = country
+    df_filtered['assessment_year'] = assessment_year
     
+    remap_area_column_names = {
+        col: col.replace('area ', '').replace('.', '_')
+        for col in area_columns
+    }
+
+    df_filtered = df_filtered.rename(columns=remap_area_column_names)
+
+    # Grab just the first element (there should only be one anyway) and return it as a dictionary
+    output_dict = df_filtered.iloc[0].to_dict()
+
+    output = CountryData(**output_dict)
+
     return output
--- v1/models.py
+++ v1/models.py
@@ -1,11 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Literal  # This helps us limit possible values
 
 class CountryData(BaseModel):
     country: str
-    assessment_year: int
-    EP_1: str 
-    EP_2: str
-    EP_3: str
+    # Only allow years between 2023-2024
+    assessment_year: int = Field(ge=2023, le=2024)
+    # Only allow specific values for assessment results
+    EP_1: Literal["Yes", "No", "Partial", ""] 
+    EP_2: Literal["Yes", "No", "Partial", ""]
+    EP_3: Literal["Yes", "No", "Partial", ""]
     CP_1: str
     CP_2: str
     CP_3: str
