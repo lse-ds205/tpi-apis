edd09d7871324c1a5dbc5e148468a52c6fc8b73a
felix-brown <felixgibsonbrown@gmail.com>
2025-02-03 11:00:01 +0000

    added lab solutions

--- v1/app.py
+++ v1/app.py
@@ -1,19 +1,30 @@
 import os
 import pandas as pd
 
-
 from fastapi import FastAPI
-from .models import CountryData 
 
-app = FastAPI()
-
-# from .models import CountryData 
-
-filepath = './data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx'
-df_assessments = pd.read_excel(filepath)
-
+df_assessments = pd.read_excel("./data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx")
 df_assessments['Assessment date'] = pd.to_datetime(df_assessments['Assessment date'])
 df_assessments['Publication date'] = pd.to_datetime(df_assessments['Publication date'])
+
+def __is_running_on_nuvolos():
+    """
+    If we are running this script from Nuvolos Cloud, 
+    there will be an environment variable called HOSTNAME
+    which starts with 'nv-'
+    """
+
+    hostname = os.getenv("HOSTNAME")
+    return hostname is not None and hostname.startswith('nv-')
+
+if __is_running_on_nuvolos():
+    # Nuvolos alters the URL of the API (likely for security reasons)
+    # Instead of https://A-BIG-IP-ADDRESS:8000/
+    # The API is actually served at https://A-BIG-IP-ADDRESS/proxy/8000/
+    app = FastAPI(root_path="/proxy/8000/")
+else:
+    # No need to set up anything else if running this on local machine
+    app = FastAPI()
 
 @app.get("/")
 async def read_root():
@@ -22,18 +33,35 @@ async def read_root():
 @app.get("/v1/country-data/{country}/{assessment_year}")
 async def get_country_data(country: str, assessment_year: int):
 
-    #only include country, year row
-    mask = (df_assessments['Assessment date'].dt.year == assessment_year) & (df_assessments['Country'] == country)
-    filtered_df = df_assessments[mask]
-    filtered_df = filtered_df.iloc[: , 1:10] #####test here to see if missing values cause errors
+    selected_row = (
+        (df_assessments["Country"] == country) &
+        (df_assessments['Assessment date'].dt.year == assessment_year)
+    )
 
-    #remove irrelevant columns
-    filtered_df = filtered_df.drop(["Id", "Assessment date", "Publication date", "Country Id", "Country"], axis=1)
+    # Filter the data
+    data = df_assessments[selected_row]
 
-    #return results as a dictionary
-    df_dict = filtered_df.iloc[0].to_dict()
+    # Selected and filter columns
+    area_columns = [col for col in df_assessments.columns if col.startswith("area")]
+    data = data[area_columns]
+    
+    # JSON does not allow for NaN or NULL. 
+    # The equivalent is just to leave an empty string instead
+    data = data.fillna('')
 
-    return {
-        "message": f"You requested data for {country} in {assessment_year}",
-        "data": df_dict
+    #Rename columns
+    data['country'] = country
+    data['assessment_year'] = assessment_year
+
+    remap_area_column_names = {
+        col: col.replace('area ', '')
+        for col in area_columns
     }
+
+    data = data.rename(columns=remap_area_column_names)
+
+    # Grab just the first element (there should only be one anyway)
+    # and return it as a dictionary
+    output_dict = data.iloc[0].to_dict()
+
+    return output_dict
