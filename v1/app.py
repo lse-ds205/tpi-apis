393e72043640a9d99b59d559daa1ecc83f14afc1
felix-brown <felixgibsonbrown@gmail.com>
2025-02-04 12:43:28 +0000

    Updated app.py and models.py with pycatch models basics

--- v1/app.py
+++ v1/app.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 
 from fastapi import FastAPI
+from .models import CountryData 
+
 
 df_assessments = pd.read_excel("./data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx")
 df_assessments['Assessment date'] = pd.to_datetime(df_assessments['Assessment date'])
@@ -30,7 +32,9 @@ else:
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/v1/country-data/{country}/{assessment_year}")
+
+
+@app.get("/v1/country-data/{country}/{assessment_year}", response_model=CountryData)
 async def get_country_data(country: str, assessment_year: int):
 
     selected_row = (
@@ -41,10 +45,14 @@ async def get_country_data(country: str, assessment_year: int):
     # Filter the data
     data = df_assessments[selected_row]
 
+    if data.empty:
+        raise HTTPException(status_code=404, 
+                            detail=f"There is no data for country: {country} and year: {assessment_year}")
+
     # Selected and filter columns
     area_columns = [col for col in df_assessments.columns if col.startswith("area")]
     data = data[area_columns]
-    
+
     # JSON does not allow for NaN or NULL. 
     # The equivalent is just to leave an empty string instead
     data = data.fillna('')
@@ -54,14 +62,17 @@ async def get_country_data(country: str, assessment_year: int):
     data['assessment_year'] = assessment_year
 
     remap_area_column_names = {
-        col: col.replace('area ', '')
+        col: col.replace('area ', '').replace('.', '_')
         for col in area_columns
     }
 
     data = data.rename(columns=remap_area_column_names)
+    output_dict = data.iloc[0].to_dict()
+
+    output = CountryData(**output_dict)
 
     # Grab just the first element (there should only be one anyway)
     # and return it as a dictionary
-    output_dict = data.iloc[0].to_dict()
+    return output
 
-    return output_dict
+
--- v1/models.py
+++ v1/models.py
@@ -3,16 +3,16 @@ from pydantic import BaseModel
 class CountryData(BaseModel):
     country: str
     assessment_year: int
-    EP1: str
-    EP2: str
-    EP3: str
-    CP1: str
-    CP2: str
-    CP3: str
-    CP4: str
-    CP5: str
-    CP6: str
-    CF1: str
-    CF2: str
-    CF3: str
-    CF4: str
+    EP_1: str 
+    EP_2: str
+    EP_3: str
+    CP_1: str
+    CP_2: str
+    CP_3: str
+    CP_4: str
+    CP_5: str
+    CP_6: str
+    CF_1: str
+    CF_2: str
+    CF_3: str
+    CF_4: str
