1529b87ec45fedd26988d3333771240699ed913b
Rui Kai <liuruikai30@gmail.com>
2025-02-04 15:48:59 +0000

    done with models

--- v1/models.py
+++ v1/models.py
@@ -1,5 +1,48 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List, Union
 
+# CountryDataResponse
+# ├── country: str
+# ├── assessment_year: int
+# └── pillars: List[Pillar]
+#     ├── name: str
+#     └── areas: List[Area]
+#         ├── name: str
+#         ├── assessment: str
+#         └── indicators: List[Indicator]
+#             ├── assessment: str
+#             └── metric: Metric
+#                 ├── name: str
+#                 └── value: str
+
+# L5
+class Metric(BaseModel):
+    name: str
+    value: str
+
+# L4
+class Indicator(BaseModel):
+    assessment: str
+    metric: Metric
+
+# L3
+class Area(BaseModel):
+    name: str
+    assessment: str
+    indicators: List[Indicator]
+
+# L2
+class Pillar(BaseModel):
+    name: str = Field(..., pattern="^(EP|CP|CF)$")
+    areas: List[Area]
+
+# L1
+class CountryDataResponse(BaseModel):
+    country: str
+    assessment_year: int
+    pillars: List[Pillar]
+
+# L0
 class CountryData(BaseModel):
     country: str
     assessment_year: int
@@ -16,12 +59,3 @@ class CountryData(BaseModel):
     CF_2: str
     CF_3: str
     CF_4: str
-
-class Indicator(BaseModel):
-    assessment: str
-    metric: Metric
-
-
-class Metric(BaseModel):
-    name: str
-    value: str
