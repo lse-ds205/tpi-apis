2622483ce4aa413a97e729ad0414f4179c787080
Alex Faith <alexfaith@Alexs-MacBook-Air.local>
2025-02-06 15:44:16 +0000

    adding literal constraints

--- v1/models.py
+++ v1/models.py
@@ -1,21 +1,23 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class CountryData(BaseModel):
     country: str
-    assessment_year: int
-    EP_1: str 
-    EP_2: str
-    EP_3: str
-    CP_1: str
-    CP_2: str
-    CP_3: str
-    CP_4: str
-    CP_5: str
-    CP_6: str
-    CF_1: str
-    CF_2: str
-    CF_3: str
-    CF_4: str
+    assessment_year: int = Field(ge=2023, le=2024)
+    # Only allow specific values for assessment results
+    EP_1: Literal["Yes", "No", "Partial", ""] 
+    EP_2: Literal["Yes", "No", "Partial", ""]
+    EP_3: Literal["Yes", "No", "Partial", ""]
+    CP_1: Literal["Yes", "No", "Partial", ""]
+    CP_2: Literal['Partial','Exempt', 'No' ,'Yes','']
+    CP_3: Literal['No' ,'Exempt' ,'Partial' ,'Yes','']
+    CP_4: Literal['Partial' ,'Exempt' ,'No','']
+    CP_5: Literal["Yes", "No", "Partial", ""] 
+    CP_6: Literal["Yes", "No", "Partial", ""]
+    CF_1: Literal['Partial','Exempt', 'No' ,'Yes','']
+    CF_2: Literal['No' ,'Exempt' ,'Partial' ,'Yes','']
+    CF_3: Literal["Yes", "No", "Partial", ""] 
+    CF_4: Literal["Not applicable", 'nan', ""] 
 
 class Indicator(BaseModel):
     assessment: str
