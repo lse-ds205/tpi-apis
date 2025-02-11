07e372f13e0a833d81fcdbb887ae164342273bff
felix-brown <felixgibsonbrown@gmail.com>
2025-02-11 12:51:40 +0000

    'Created prototype function (V2 prototype) in jupyter notebook'

--- v1/models.py
+++ v1/models.py
@@ -1,34 +1,25 @@
 from pydantic import BaseModel, Field
-from typing import Literal
+from typing import Optional, Literal, List, Dict
+
+class Metrics(BaseModel):
+    name: str
+    value: str
+
+class Indicators(BaseModel):
+    name: str
+    assessment: str
+    metrics: Optional[list[Metrics]] = None
 
 class Area(BaseModel):
-    indicators: dict[str, str]
+    name: str
+    assessment: Optional[str] = None
+    indicators: List[Indicators]
+
+class Pillars(BaseModel):
+    name: str
+    areas: List[Area]
 
 class CountryData(BaseModel):
     country: str
-    assessment_year: int
-    EP: Area
-    CP: Area
-    CF: Area
-
-#class CountryData(BaseModel):
-#    country: str
-
-    # Only allow years between 2023-2024
-#   assessment_year: int = Field(ge=2023, le=2024)
-
-    # Only allow specific values for assessment results
-#    EP_1: Literal["Yes", "No", "Partial", ""] 
-#    EP_2: Literal["Yes", "No", "Partial", ""]
- #   EP_3: Literal["Yes", "No", "Partial", ""]
-#
- #   CP_1: Literal['Yes', 'No', 'Partial', '']
-  #  CP_2: Literal['Partial', 'Exempt', 'No', 'Yes', '']
-    #CP_3: Literal['No', 'Exempt', 'Partial', 'Yes', '']
-   # CP_4: Literal['Partial', 'Exempt', 'No', ""]
-#    CP_5: Literal['Partial', 'Yes', 'No', ""]
- #   CP_6: Literal['Partial', 'No', 'Yes', ""]
-  #  CF_1: Literal['Partial', 'Exempt', 'No', 'Yes', '']
-   # CF_2: Literal['Exempt', 'Partial', 'No', 'Yes', '']
-    #CF_3: Literal['Yes', 'No', 'Partial', '']
- #   CF_4: Literal['Not applicable','']
+    assessment_year: int = Field(ge=2023, le=2024)
+    pillars: Optional[List[Pillars]]
