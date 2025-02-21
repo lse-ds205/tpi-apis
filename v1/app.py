fb03c147b6150f111b215b3a67032b02e50a9acc
GAURVI27 <147526356+GAURVI27@users.noreply.github.com>
2025-02-21 14:33:04 +0000

    Updated app.py to fix data filtering and API response

--- v1/app.py
+++ v1/app.py
@@ -1,34 +1,12 @@
 import os
 import pandas as pd
-
 from typing import List
-
 from fastapi import FastAPI, HTTPException
-
 from .models import CountryData, Metric
 
-df_assessments = pd.read_excel("./data/TPI ASCOR data - 13012025/ASCOR_assessments_results.xlsx")
+df_assessments = pd.read_excel("./data/TPI ASCOR data - 18022025/ASCOR_assessments_results.xlsx")
 df_assessments['Assessment date'] = pd.to_datetime(df_assessments['Assessment date'])
 df_assessments['Publication date'] = pd.to_datetime(df_assessments['Publication date'])
-
-def __is_running_on_nuvolos():
-    """
-    If we are running this script from Nuvolos Cloud, 
-    there will be an environment variable called HOSTNAME
-    which starts with 'nv-'
-    """
-
-    hostname = os.getenv("HOSTNAME")
-    return hostname is not None and hostname.startswith('nv-')
-
-if __is_running_on_nuvolos():
-    # Nuvolos alters the URL of the API (likely for security reasons)
-    # Instead of https://A-BIG-IP-ADDRESS:8000/
-    # The API is actually served at https://A-BIG-IP-ADDRESS/proxy/8000/
-    app = FastAPI(root_path="/proxy/8000/")
-else:
-    # No need to set up anything else if running this on local machine
-    app = FastAPI()
 
 @app.get("/")
 async def read_root():
@@ -38,8 +16,8 @@ async def read_root():
 async def get_country_data(country: str, assessment_year: int):
 
     selected_row = (
-        (df_assessments["Country"] == country) &
-        (df_assessments['Assessment date'].dt.year == assessment_year)
+        (df_assessments["Country"] == United Kingdom) &
+        (df_assessments['Assessment date'].dt.year == 2024)
     )
 
     # Filter the data
@@ -74,44 +52,3 @@ async def get_country_data(country: str, assessment_year: int):
     # Grab just the first element (there should only be one anyway)
     # and return it as a dictionary
     return output
-
-@app.get("/v1/country-metrics/{country}/{assessment_year}", response_model=List[Metric])
-async def get_country_metrics(country: str, assessment_year: int):
-
-    selected_row = (
-        (df_assessments["Country"] == country) &
-        (df_assessments['Assessment date'].dt.year == assessment_year)
-    )
-
-    # Filter the data
-    data = df_assessments[selected_row]
-
-    if data.empty:
-        raise HTTPException(status_code=404, 
-                            detail=f"There is no data for country: {country} and year: {assessment_year}")
-
-    # Select just the metrics
-    metric_columns = [col for col in df_assessments.columns 
-                      if col.startswith('metric')]
-    data = data[metric_columns]
-
-    # JSON does not allow for NaN or NULL. 
-    # The equivalent is just to leave an empty string instead
-    data = data.fillna('')
-
-    remap_area_column_names = {
-        col: col.replace('metric ', '')
-        for col in metric_columns
-    }
-
-    data = data.rename(columns=remap_area_column_names)
-
-    data_as_dict = data.iloc[0].to_dict()
-
-    list_metrics = []
-    for name, value in data_as_dict.items():
-        individual_metric = Metric(name=name, value=value)
-        list_metrics.append(individual_metric)
-    # Grab just the first element (there should only be one anyway)
-    # and return it as a dictionary
-    return list_metrics
