19b6785e343ab662880e8ea6588067e57f3651f8
GAURVI27 <147526356+GAURVI27@users.noreply.github.com>
2025-02-22 11:45:17 +0000

    Add basic Pydantic model for API response

--- v1/app.py
+++ v1/app.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from typing import List
 from fastapi import FastAPI, HTTPException
-from .models import CountryData, Metric
+from .models import CountryData
 app = FastAPI()
 
 
@@ -10,12 +10,8 @@ df_assessments = pd.read_excel("./data/TPI ASCOR data - 13012025/ASCOR_assessmen
 df_assessments['Assessment date'] = pd.to_datetime(df_assessments['Assessment date'])
 df_assessments['Publication date'] = pd.to_datetime(df_assessments['Publication date'])
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
 @app.get("/v1/country-data/{country}/{assessment_year}", response_model=CountryData)
-async def get_country_data(country: str, assessment_year: int):
+async def get_country_data(country: str, assessment_year: int) -> CountryData:
 
     selected_row = (
         (df_assessments["Country"] == country) &
@@ -25,14 +21,10 @@ async def get_country_data(country: str, assessment_year: int):
     # Filter the data
     data = df_assessments[selected_row]
 
-    if data.empty:
-        raise HTTPException(status_code=404, 
-                            detail=f"There is no data for country: {country} and year: {assessment_year}")
-
     # Selected and filter columns
     area_columns = [col for col in df_assessments.columns if col.startswith("area")]
     data = data[area_columns]
-
+    
     # JSON does not allow for NaN or NULL. 
     # The equivalent is just to leave an empty string instead
     data = data.fillna('')
@@ -47,10 +39,11 @@ async def get_country_data(country: str, assessment_year: int):
     }
 
     data = data.rename(columns=remap_area_column_names)
+
+    # Grab just the first element (there should only be one anyway)
+    # and return it as a dictionary
     output_dict = data.iloc[0].to_dict()
 
     output = CountryData(**output_dict)
 
-    # Grab just the first element (there should only be one anyway)
-    # and return it as a dictionary
     return output
