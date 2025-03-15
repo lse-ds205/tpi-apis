23a9c2a1c532c7f57e82064d77b24fa6acbfcfed
Jon Cardoso-Silva <jonathan.car.silva@gmail.com>
2025-03-15 12:28:44 +0000

    ♻️ Align data model with Bryan's version (currently on `main`)

--- v1/models.py
+++ v1/models.py
@@ -1,43 +1,33 @@
 from pydantic import BaseModel, Field
-from typing import Literal, List, Optional
+from typing import Union, List, Optional
 
 class MetricSource(BaseModel):
     source_name: Optional[str]
-
 
 class Metric(BaseModel):
     name: str
     value: str
     source: Optional[MetricSource] = None
-   
+
 class IndicatorSource(BaseModel):
     source_name: Optional[str]
 
 class Indicator(BaseModel):
     name: str
-    metrics: Optional[List[Metric]]
+    assessment: str
+    metrics: List[Union[Metric, str]] = Field(default_factory=list)
     source: Optional[IndicatorSource] = None
-    
 
 class Area(BaseModel):
     name: str
-    indicators: Optional[List[Indicator]]
+    assessment: str
+    indicators: List[Indicator]
 
-class CountryData(BaseModel):
+class Pillar(BaseModel):
+    name: str = Field(..., pattern="^(EP|CP|CF)$")
+    areas: List[Area]
+
+class CountryDataResponse(BaseModel):
     country: str
     assessment_year: int = Field(ge=2023, le=2024)
-    areas: Optional[List[Area]]
-    # EP_1: Literal["Yes", "No", "Partial", ""] 
-    # EP_2: Literal["Yes", "No", "Partial", ""]
-    # EP_3: Literal["Yes", "No", "Partial", ""]
-    # CP_1: Literal["Yes", "No", "Partial", ""]
-    # CP_2: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CP_3: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CP_4: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CP_5: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CP_6: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CF_1: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CF_2: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CF_3: Literal["Yes", "No", "Partial", "Exempt", ""]
-    # CF_4: Literal["Yes", "No", "Partial", "Exempt", ""]
-
+    pillars: List[Pillar]
